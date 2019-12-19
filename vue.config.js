3948a901885406c3ab6757ea7aa01b1e4465552c
Troy Latchman <tlatchman@jacada.com>
2019-12-19 17:18:40 -0500

    Configure webpack dev server to route all requests (including websockets) to localhost.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+// flag that indicates if the webpage (Interact UAD app) is secure - https vs http
+const https = false;
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  publicPath: https ? "https://localhost:8080/" : "http://localhost:8080/", // fixes runtime require/chunks
+  devServer: {
+    disableHostCheck: https ? true : false, // may need to find a better solution for https
+    https: https ? true : false,
+    public: "localhost:8080", // access localhost resources, even when proxied
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  }
 };
